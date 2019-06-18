8afc5c5f28ec1264520b7b13c2a867a46ffe3ea3
zakree <957681484@qq.com>
2019-06-18 22:00:53 +0800

    update

--- src/MyTree/GuessAnimal.java
+++ src/MyTree/GuessAnimal.java
@@ -73,7 +73,7 @@ public class GuessAnimal {
 
     }
 
-    public static void main(String[] args) {
+    public static void bootstrap(){
         BTNode root;
         root = beginningTree();
 
--- src/TestAnyway.java
+++ src/TestAnyway.java
@@ -1,6 +1,23 @@
-//ÁÙÊ±²âÊÔÀà
+import java.util.Arrays;
+
+//ä¸´æ—¶æµ‹è¯•ç±»
 public class TestAnyway {
     public static void main(String[] args) {
+        int[] array={3,4,5,1,2};
+        int min=0;
+        int max=array.length-1;
+        while(min<max){
+            int mid=(min+max)/2;
+            if(min==max-1){
+                break;
+            }else if(array[mid]>array[max]){
+                min=mid;
+            }else if(array[mid]<array[max]){
 
+                max=mid;
+            }
+        }
+        System.out.println(array[max]);
     }
+
 }
